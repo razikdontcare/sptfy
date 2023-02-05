3b7364548aa44e74ae8b806b292285a3adc6ffc3
razikdontcare <gamersia4@gmail.com>
2023-02-05 20:16:42 +0800

    fixing layout

--- pages/index.jsx
+++ pages/index.jsx
@@ -79,7 +79,7 @@ export default function Home() {
       </Head>
 
       <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="hero-content">
           <div className="text-center lg:text-left">
             <h1 className="text-5xl font-bold">sptfy.tech</h1>
             <p className="py-6">Free Spotify URL Shortener just for you.</p>
