e719d89ba8d55c9aee9e46399c82cdadd6964a8d
razikdontcare <gamersia4@gmail.com>
2023-02-05 20:28:07 +0800

    fixing layout 2nd

--- pages/index.jsx
+++ pages/index.jsx
@@ -80,94 +80,96 @@ export default function Home() {
 
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content">
-          <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">sptfy.tech</h1>
-            <p className="py-6">Free Spotify URL Shortener just for you.</p>
-          </div>
-          {username != "" && (
-            <div className="alert shadow-lg">
-              <div>
-                <span className="font-bold">Preview: </span>
-                <span>sptfy.tech/{username}</span>
-              </div>
+          <div className="max-w-md">
+            <div className="text-center lg:text-left">
+              <h1 className="text-5xl font-bold">sptfy.tech</h1>
+              <p className="py-6">Free Spotify URL Shortener just for you.</p>
             </div>
-          )}
-          {showError && (
-            <div className="alert alert-error shadow-lg">
-              <div>
-                <span>
-                  URL with that id already exists. use another id or left it
-                  blank for random id
-                </span>
+            {username != "" && (
+              <div className="alert shadow-lg">
+                <div>
+                  <span className="font-bold">Preview: </span>
+                  <span>sptfy.tech/{username}</span>
+                </div>
               </div>
-            </div>
-          )}
-          {showUrl && (
-            <div className="alert alert-success shadow-lg">
-              <div>
-                <span className="font-bold">Success!</span>
-                <span>
-                  Your URL is{" "}
-                  <a className="link link-hover" href={"/" + urlId}>
-                    sptfy.tech/{urlId}
-                  </a>
-                </span>
-              </div>
-            </div>
-          )}
-          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <div className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Your Spotify Link</span>
-                </label>
-                <input
-                  value={longUrl}
-                  onChange={(e) => setLongUrl(e.target.value)}
-                  type="text"
-                  className="input input-bordered"
-                  required
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">
-                    Your Custom ID / Spotify Username {"(Optional)"}
+            )}
+            {showError && (
+              <div className="alert alert-error shadow-lg">
+                <div>
+                  <span>
+                    URL with that id already exists. use another id or left it
+                    blank for random id
                   </span>
-                </label>
-                <input
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                  type="text"
-                  className="input input-bordered"
-                />
+                </div>
               </div>
-              <div className="form-control mt-6">
-                <button
-                  disabled={longUrl == ""}
-                  onClick={shorten}
-                  className="btn btn-primary"
-                >
-                  Shorten!
-                </button>
+            )}
+            {showUrl && (
+              <div className="alert alert-success shadow-lg">
+                <div>
+                  <span className="font-bold">Success!</span>
+                  <span>
+                    Your URL is{" "}
+                    <a className="link link-hover" href={"/" + urlId}>
+                      sptfy.tech/{urlId}
+                    </a>
+                  </span>
+                </div>
+              </div>
+            )}
+            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+              <div className="card-body">
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Your Spotify Link</span>
+                  </label>
+                  <input
+                    value={longUrl}
+                    onChange={(e) => setLongUrl(e.target.value)}
+                    type="text"
+                    className="input input-bordered"
+                    required
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">
+                      Your Custom ID / Spotify Username {"(Optional)"}
+                    </span>
+                  </label>
+                  <input
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    type="text"
+                    className="input input-bordered"
+                  />
+                </div>
+                <div className="form-control mt-6">
+                  <button
+                    disabled={longUrl == ""}
+                    onClick={shorten}
+                    className="btn btn-primary"
+                  >
+                    Shorten!
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
 
-          <footer className="footer-center p-4 text-base-content">
-            <div>
-              <p>
-                made with ♥️ by{" "}
-                <a
-                  className="link link-hover"
-                  href="https://www.instagram.com/razikdontcare"
-                >
-                  razikdontcare
-                </a>
-                .
-              </p>
-            </div>
-          </footer>
+            <footer className="footer-center p-4 text-base-content">
+              <div>
+                <p>
+                  made with ♥️ by{" "}
+                  <a
+                    className="link link-hover"
+                    href="https://www.instagram.com/razikdontcare"
+                  >
+                    razikdontcare
+                  </a>
+                  .
+                </p>
+              </div>
+            </footer>
+          </div>
         </div>
       </div>
     </>
